b37fe27da3153c52ba0aa87d67119f7abb67d86a
DuyTrung8403 <124582167+DuyTrung8403@users.noreply.github.com>
2026-06-01 20:06:15 +0700

    Add files via upload

--- ss6_cau1.sql
+++ ss6_cau1.sql
@@ -1,18 +1,18 @@
 CREATE TABLE Product (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100),
-    category VARCHAR(50),
-    price NUMERIC(10,2),
-    stock INT
+                         id SERIAL PRIMARY KEY,
+                         name VARCHAR(100),
+                         category VARCHAR(50),
+                         price NUMERIC(10,2),
+                         stock INT
 );
 
 -- 1. Thêm 5 sản phẩm vào bảng bằng lệnh INSERT
 INSERT INTO Product (name, category, price, stock) VALUES
-('iPhone 15 Pro Max', 'Điện tử', 29500000.00, 15),
-('Chuột Logitech G102', 'Điện tử', 450000.00, 50),
-('Bàn phím cơ AKKO', 'Điện tử', 1500000.00, 8),
-('Tủ lạnh Samsung 400L', 'Gia dụng', 12500000.00, 5),
-('Quạt cây Panasonic', 'Gia dụng', 2200000.00, 20);
+                                                       ('iPhone 15 Pro Max', 'Điện tử', 29500000.00, 15),
+                                                       ('Chuột Logitech G102', 'Điện tử', 450000.00, 50),
+                                                       ('Bàn phím cơ AKKO', 'Điện tử', 1500000.00, 8),
+                                                       ('Tủ lạnh Samsung 400L', 'Gia dụng', 12500000.00, 5),
+                                                       ('Quạt cây Panasonic', 'Gia dụng', 2200000.00, 20);
 
 
 -- 2.Hiển thị danh sách toàn bộ sản phẩm
